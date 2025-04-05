2d2da2c7b4df87aefc59a68669e25b7316c9b6cb
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2025-04-05 22:01:26 +0300

    feat: add funcs usersInput and getValue

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
 	const usdToEur = 0.91
 	const usdToRub = 84.44
 	const eurToRub = usdToRub / usdToEur
+
+	input := usersInput()
+	fmt.Println(input)
+}
+
+func usersInput() string {
+	var input string
+	fmt.Scan(&input)
+	return input
+}
+
+func getValue(amount, from, to string) {
+
 }
